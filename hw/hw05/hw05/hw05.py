e662dc33ca6249fe2abfce268cde2bae007f5f96
htwzxwj <769856576@qq.com>
2025-01-03 11:41:17 +0800

    实现 hailstone 函数，生成从 n 开始的海尔斯通序列

--- hw/hw05/hw05/hw05.py
+++ hw/hw05/hw05/hw05.py
@@ -10,6 +10,13 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    yield n
+    if n == 1:
+        yield from hailstone(n)
+    elif n % 2 == 0:
+        yield from hailstone(n // 2)
+    else:
+        yield from hailstone(3 * n + 1) 
 
 
 def merge(a, b):
