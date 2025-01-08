6862d9cfd7cdfc177b997da24a3ec633a3d5b3f6
htwzxwj <769856576@qq.com>
2025-01-08 10:19:50 +0800

    完善 deep_map_mut 和 two_list 函数的实现，添加递归处理和链表构建逻辑

--- hw/hw06/hw06/hw06.py
+++ hw/hw06/hw06/hw06.py
@@ -153,6 +153,13 @@ def deep_map_mut(func, s):
     <9 <16> 25 36>
     """
     "*** YOUR CODE HERE ***"
+    if s is Link.empty:
+        return None
+    elif isinstance(s.first, Link):
+        deep_map_mut(func, s.first)
+    else:
+        s.first = func(s.first)
+    deep_map_mut(func, s.rest)
 
 
 def two_list(vals, counts):
@@ -174,6 +181,13 @@ def two_list(vals, counts):
     Link(1, Link(1, Link(3, Link(3, Link(2)))))
     """
     "*** YOUR CODE HERE ***"
+    result = Link(None)
+    p = result
+    for i in range(len(vals)):
+        for _ in range(counts[i]):
+            p.rest = Link(vals[i])
+            p = p.rest
+    return result.rest
 
 
 class Link:
