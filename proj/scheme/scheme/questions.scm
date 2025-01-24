b2f1078494684a4990bba534833cd8f210a1bec3
htwzxwj <769856576@qq.com>
2025-01-24 16:28:59 +0800

    完成proj scheme

--- proj/scheme/scheme/questions.scm
+++ proj/scheme/scheme/questions.scm
@@ -7,8 +7,12 @@
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
-  )
+  (define (helper s index)
+    (if (null? s)  ; Base case: if the list is empty, return an empty list
+        '()  ; Return an empty list
+        (cons (list index (car s))  ; Pair the index with the current element
+              (helper (cdr s) (+ index 1)))))  ; Recur with the rest of the list and increment the index
+  (helper s 0))  ; Start the helper with index 0
   ; END PROBLEM 15
 
 ;; Problem 16
@@ -17,8 +21,13 @@
 ;; the merged lists.
 (define (merge ordered? s1 s2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
-  )
+  (cond
+    ((null? s1) s2)  ; 如果 s1 为空，返回 s2
+    ((null? s2) s1)  ; 如果 s2 为空，返回 s1
+    (else
+     (if (ordered? (car s1) (car s2))  ; 比较 s1 和 s2 的第一个元素
+         (cons (car s1) (merge ordered? (cdr s1) s2))  ; s1 的第一个元素较小，递归合并剩余部分
+         (cons (car s2) (merge ordered? s1 (cdr s2)))))))  ; s2 的第一个元素较小，递归合并剩余部分
   ; END PROBLEM 16
 
 ;; Optional Problem 2
